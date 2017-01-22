35228cfac8132be4d6125d27aef72044350aa857
c2pig <c2pigisme@yahoo.com>
2017-01-22 22:18:04 +0800

    add generator compose

--- generators/index.js
+++ generators/index.js
@@ -37,49 +37,25 @@ module.exports = Generator.extend({
       default : 'code'
     },
   ];
-
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
     }.bind(this));
   },
-
+  configuring: function() {
+    this.composeWith(require.resolve('review-service-baseline/generators/app'),  this.props);
+  },
   writing: function () {
     let YAML = require('js-yaml');
-    let basePath = 'base';
-    this.props.basePath = basePath;
 
-    const files = {
-      'common/**':'common',
-      'ingest/**':'ingest',
-      'validation/**':'validation',
-      'moderation/**':'moderation',
-      'view/**':'view',
-      'config.js':'config.js',
-    };
-
-    const templates = {
-      'serverless.yml': '../serverless.yml',
-      'package.json': '../package.json',
-      '.gitignore':'../.gitignore',
-      'ingest/functions.yml': '../tmp/ingest.func.tmp',
-      'validation/functions.yml': '../tmp/validation.func.tmp',
-      'moderation/functions.yml': '../tmp/moderation.func.tmp',
-      'view/functions.yml': '../tmp/view.func.tmp'
-    };
-
+    // TODO: dont quite like this, can we do { tmp/**/*.tmp : ../serverless.yml }
     const partials = {
-      'tmp/ingest.func.tmp' : '../serverless.yml',
-      'tmp/validation.func.tmp' : '../serverless.yml',
-      'tmp/moderation.func.tmp' : '../serverless.yml',
-      'tmp/view.func.tmp' : '../serverless.yml'
+      'tmp/ingest.func.tmp' : 'serverless.yml',
+      'tmp/validation.func.tmp' : 'serverless.yml',
+      'tmp/moderation.func.tmp' : 'serverless.yml',
+      'tmp/view.func.tmp' : 'serverless.yml'
     };
 
-    const normalCopy = (path, dest) => {
-      this.fs.copy(this.templatePath(path), this.destinationPath(dest));
-    }
-
-    const templateCopy = (path, dest) => this.fs.copyTpl(this.templatePath(path), this.destinationPath(dest), this.props);
 
     const contentMerge = (fromFile, toFile) => {
 
@@ -102,25 +78,20 @@ module.exports = Generator.extend({
 
     const fileOps = ((action, files) => {
       Object.keys(files).forEach((function(path) {
-        const dest = basePath + '/' + files[path] || path;
+        const dest = files[path] || path;
         action(path, dest);
       }).bind(this));
     }).bind(this);
 
-    fileOps(normalCopy, files);
-    fileOps(templateCopy, templates);
     fileOps(translatedTemplateMerge, partials);
     console.log(`creating dir:${this.props.codeDir}`);
     mkdirp(this.props.codeDir||".");
   },
   install: function () {
-    this.installDependencies({bower:true, npm:false});
-  },
-  end: function() {
+    console.log('clean up tmp/ ...');
     rimraf("tmp/", (x) => {
-      if(x) {
-        console.log(x);
-      }
+      if(x) {  console.log(x); }
     });
+    this.installDependencies({bower:false, npm:true});
   }
 });
